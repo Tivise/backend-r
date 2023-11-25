62a2882be9dd78db2c05ae002e8275139b30b28f
Tivise <tivise@hotmail.fr>
2023-11-25 18:23:21 +0100

    FIX - Added try catch to protect the middleware GetSteamuserInformation

--- controllers/steam.js
+++ controllers/steam.js
@@ -2,7 +2,12 @@ const axios = require("axios");
 const API_KEY = '---------------';
 
 exports.GetSteamUserInformation = async (req, res) => {
-  const steamID = req.query.steamID;
-  const response = await axios.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+ API_KEY + '&steamids=' + steamID);
-  res.json(response.data.response.players[0]); // Return the data from St eam API as JSON
+  try{
+    const steamID = req.query.steamID;
+    const response = await axios.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='+ API_KEY + '&steamids=' + steamID);
+    res.status(200).json(response.data.response.players[0]); // Return the data from Steam API as JSON
+  }
+  catch (err){
+    res.status(500).json({'msg': err});
+  }
 }
